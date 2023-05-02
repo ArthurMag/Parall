bed46af7cffd616919d390a870f3e05216ecce57
ArthurMag <magomedovrulit@mail.ru>
2023-05-02 11:42:41 +0300

    Removed need for mutex from Threads1.c, improves time

--- Threads1.c
+++ Threads1.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <pthread.h>
 
-pthread_mutex_t mutex;
 
 void randomizeMatrix(int **matrix, int row, int collumn) {
 	for (int i = 0; i < row; ++i) {
@@ -27,7 +26,6 @@ int** freeMatrix(int **matrix, int row) {
                 free(matrix[i]);
         }
         free(matrix);
-	/*matrix = NULL;*/
 	return matrix;
 }
 
@@ -36,30 +34,30 @@ struct param {
         int* end;
 		int*** matrixA;
 		int*** matrixB;
-		int*** matrixC;
 		int* M;
 		int* L;
 };
 
-void do_count(int M, int L, int start, int end, int** A, int** B, int** C) {
+void do_count(int M, int L, int start, int end, int** A, int** B, int** locC) {
 	int LocalSum = 0;
 	for (int i = 0; i < M; ++i) {
 		for (int j = 0; j < L; ++j) {
 			for (int k = start; k < end; ++k) {
-					LocalSum = LocalSum + A[i][k] * B[k][j];
-				}
-			pthread_mutex_lock(&mutex);
-			C[i][j] = C[i][j] + LocalSum;
-			pthread_mutex_unlock(&mutex);
-			LocalSum = 0;
+				LocalSum = LocalSum + A[i][k] * B[k][j];
 			}
-		}		
+			locC[i][j] = locC[i][j] + LocalSum;
+			LocalSum = 0;
+		}
+	}		
 }
 
 void* do_thread(void* arg) {
 	struct param *my_param = (struct param*)arg;
-	do_count(*(*my_param).M, *(*my_param).L, *(*my_param).start, *(*my_param).end, *(*my_param).matrixA, *(*my_param).matrixB, *(*my_param).matrixC);
+	int **LocalC;
+	LocalC = mallocMatrix(LocalC, *(*my_param).M, *(*my_param).L);
+	do_count(*(*my_param).M, *(*my_param).L, *(*my_param).start, *(*my_param).end, *(*my_param).matrixA, *(*my_param).matrixB, LocalC);
 	free(arg);
+	return (void***) LocalC;
 }
 
 int get_something(int diff, int thread_num, int bin) {
@@ -120,17 +118,21 @@ void main() {
 		(*arg[i]).end = &endA[i];
 		(*arg[i]).matrixA = &mMN;
 		(*arg[i]).matrixB = &mNL;
-		(*arg[i]).matrixC = &C;
 		(*arg[i]).M = &M;
 		(*arg[i]).L = &L;
-		printf("start = %d, end = %d\n", *(*arg[i]).start, *(*arg[i]).end);
 		pthread_create(&th[i], NULL, &do_thread, (void*) arg[i]);
 	}
 
-	for (int i = 0; i < p; i++) {
-		pthread_join(th[i], NULL);
+	for (int k = 0; k < p; k++) {
+		int** LclC;
+		pthread_join(th[k], (void **) &LclC);
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < L; j++) {
+				C[i][j] = C[i][j] + LclC[i][j];
+			}
+		}
+		freeMatrix(LclC, M);
 	}
-	pthread_mutex_destroy(&mutex);
 	struct timespec end;
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
 
@@ -143,7 +145,6 @@ void main() {
 			Summ = Summ + C[i][j];
 		}
 	}
-
 	printf("Summ: %d\n", Summ); //sum check if needed
 
 	mMN = freeMatrix(mMN, M);
